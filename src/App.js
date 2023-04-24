767b3f946286059b98e1044379309d07353cc900
marcinoles87 <marcinoles87@gmail.com>
2023-04-24 16:15:36 +0200

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import React,{useEffect, useState} from 'react'
 import './App.css';
 
 const App = () => {
+
+  let [recipe,setRecipe] = useState()
+
+
+  useEffect( () => {
+    fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/?results=5')
+  })
+
   return(
     <div>
       <h1>Recipe Mrc </h1>
