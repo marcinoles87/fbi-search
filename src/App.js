de866d6f6acd031bedc84a942b1b854ffe643b82
marcinoles87 <marcinoles87@gmail.com>
2023-04-24 16:20:36 +0200

    add fetch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ const App = () => {
 
 
   useEffect( () => {
-    fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/?results=5')
+    fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/?results=1')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      
+    })
   })
 
   return(
