2f24ae2fb5fb145a03350243dbc2e180516b5ef0
marcinoles87 <marcinoles87@gmail.com>
2023-04-27 15:42:22 +0200

    add css

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import './recipecard.css'
+
 const RecipeCard = (props) => {
 
     const {name , img , text} = props
 
     
     return(
-        <div>
+        <div className='card'>
             <h1>Future Info </h1>
             <img src={img} alt='img'></img>
             <ul>
